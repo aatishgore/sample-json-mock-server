b3b734b0d9d677f99162084687202e0e96f0f76e
Aatish Gore <>
2020-04-30 21:43:47 +0530

    feat: adding dynamic routes

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sample-json-mock-server
+
 Generate mock api based on json structure
+
+Define different type of api in src/mock-api folder
+
+Sample
+
+````{
+  "uri": "api/incident/:id",
+  "method": "get",
+  "response": {
+    "status": true,
+    "data": {
+      "event_name": "Sample Event",
+      "event_source": "Sample source",
+      "event_details": "Sample Details",
+      "event_type": "Warning"
+    }
+  }
+}```
+````
--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ const server = require("mock-json-server");
 var notification = require("./mock-api/notification.json");
 var incidentList = require("./mock-api/incidentList.json");
 var incident = require("./mock-api/incident.json");
-const app = server(
-  {
-    "api/notification": {
-      get: notification,
-    },
-    "api/incident/": {
-      get: incidentList,
-    },
-    "api/incident/:id": {
-      get: incident,
-    },
-  },
-  8001,
-  "localhost"
-); // Start the server with a JSON object;
+
+const mockFolder = __dirname + "/mock-api/";
+let serverData = {};
+const fs = require("fs");
+fs.readdirSync(mockFolder).forEach((file) => {
+  // console.log(mockFolder + file);
+  let data = require(mockFolder + file);
+  // console.log(data);
+  let api = {};
+  api[data.method] = data.response;
+  serverData[data.uri] = api;
+});
+
+const app = server(serverData, 8001, "localhost"); // Start the server with a JSON object;
 
 // Start the server;
 app.start();
